ba4b2b9c4689ae8bf6dd330b67c8c4eec6e11a99
rmit-s3834830-Yating-Song <s3834830@student.rmit.edu.au>
2022-06-24 00:06:26 +1000

    add style and logo on navbar

--- src/components/navigation/navigation.component.js
+++ src/components/navigation/navigation.component.js
@@ -1,15 +1,19 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/plant-flower-outline-svgrepo-com.svg";
+import "./navigation.style.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="nav-link-container">
         <div className="logo-container">
-          <div>Logo</div>
+          <Logo />
         </div>
         <div className="links-container">
-          <Link to="/aboutus">About Us</Link>
+          <Link className="nav-link" to="/aboutus">
+            About Us
+          </Link>
         </div>
       </div>
 
