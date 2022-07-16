cc71be767779b185e8c7058a84282f59ef6e7bc0
rmit-s3834830-Yating-Song <s3834830@student.rmit.edu.au>
2022-07-17 00:34:57 +1000

    add page to display all items; add css to items page

--- src/components/shopping/shopping.component.js
+++ src/components/shopping/shopping.component.js
@@ -1,5 +1,20 @@
+import shoppingItem from "../../shopping-item.json";
+import "./shopping.style.scss";
+
 const Shopping = () => {
-  return <div>items</div>;
+  return (
+    <div className="shopping-item-container">
+      {shoppingItem.map((item) => {
+        return (
+          <div className="item-card" key={item.id}>
+            <img src={item.imageUrl} alt={item.name} />
+            <div>{item.name}</div>
+            <div>${item.price}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Shopping;
