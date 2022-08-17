bff62ea97c5add604d5898a6d7e230d14a6b4802
Yating Song <yatingsong7@gmail.com>
2022-08-17 23:46:13 +1000

    add scroll to top effect

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Homepage from "./components/homepage/homepage.component";
 import SignIn from "./components/sign-in/sign-in.component";
 import SignUp from "./components/sign-up/sign-up.component";
 import ShopItems from "./routes/Shop-items.route";
+import ScrollToTop from "./components/scrollToTop/scroll-to-top.component";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HeaderAndFooterWrapper />}>
-        <Route index element={<Homepage />} />
-        <Route path="items/*" element={<ShopItems />} />
-        <Route path="aboutus" element={<AboutUs />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<HeaderAndFooterWrapper />}>
+          <Route index element={<Homepage />} />
+          <Route path="items/*" element={<ShopItems />} />
+          <Route path="aboutus" element={<AboutUs />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </ScrollToTop>
   );
 };
 
